4b9f4dadc0a204f7457b4e8ab6dcb7840f6a59d6
Maricvs <49585403+Maricvs@users.noreply.github.com>
2019-10-01 22:16:14 +0300

    removing files

--- 1Task.rb
+++
@@ -1,8 +0,0 @@
-# to start working with Watir gem I need to require it
-require 'watir'
-
-# Line 5 launches the browser immediately with the bank interface,
-# line 6 clicks on the banking demo page, because there is no direct link
-  b = Watir::Browser.start "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK"
-  b.link(id: 'demo-link').click
-  sleep 5
