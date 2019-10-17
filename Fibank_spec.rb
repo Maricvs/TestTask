3cb6376032bf8016935904b557b4edd534031f5c
maricvs <mcmaric@rambler.ru>
2019-10-17 13:34:03 +0300

    Extra Done) (code optimization)

--- Fibank_spec.rb
+++ Fibank_spec.rb
@@ -4,37 +4,39 @@ require 'rspec'
 require_relative './Fibank'
 
 describe Fibank do
-  let(:account) { Fibank.new.parse_accounts(html) }
-  let(:html) { Nokogiri::HTML(File.open("accounts.html")) }
+  describe '#parse_accounts' do
+    let(:account) { Fibank.new.parse_accounts(html) }
+    let(:html) { Nokogiri::HTML(File.open("accounts.html")) }
 
-  it 'returns Accounts count' do
-    expect(account.count).to eq(2)
-  end
+    it 'returns Accounts count' do
+      expect(account.count).to eq(2)
+    end
 
-  it 'parse Accounts data example' do
-    expect(account[0].to_hash).to eq({
-      :name =>      "fibank EUR",
-      :currency =>  "EUR",
-      :balance =>   0.0
-    })
+    it 'parse Accounts data example' do
+      expect(account[0].to_hash).to eq({
+        :name =>      "fibank EUR",
+        :currency =>  "EUR",
+        :balance =>   0.0
+      })
     end
   end
 
-describe Fibank do
-  let(:account) { Account.new("example", "CUR", 0.0) }
-  let(:html) { Nokogiri::HTML(File.open("transactions.html")) }
+  describe '#parse_transactions' do
+    let(:account) { Account.new("example", "CUR", 0.0) }
+    let(:html) { Nokogiri::HTML(File.open("transactions.html")) }
 
-  it 'returns Transactions count' do
-    transaction = Fibank.new.parse_transactions(account, html)
-    expect(account.transactions.count).to eq(4)
-  end
+    it 'returns Transactions count' do
+      Fibank.new.parse_transactions(account, html)
+      expect(account.transactions.count).to eq(4)
+    end
 
-  it 'parse Transactions dara example' do
-    transaction = Fibank.new.parse_transactions(account, html)
-    expect(account.transactions[0].to_hash).to eq({
-      :amount =>      1000.0,
-      :date =>        "23/08/2019",
-      :description => "Received transfer in BGN"
-    })
+    it 'parse Transactions dara example' do
+      Fibank.new.parse_transactions(account, html)
+      expect(account.transactions[0].to_hash).to eq({
+        :amount =>      1000.0,
+        :date =>        "23/08/2019",
+        :description => "Received transfer in BGN"
+      })
+    end
   end
 end
